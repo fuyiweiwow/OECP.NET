3ec8a38852822a0d6404cb6f958022959c60d12a
fuyiwei <601556811@qq.com>
2020-07-30 13:44:57 +0800

    1. layerLegend and layeraction control demo

--- OECP.NET/ControlStation/OECPLayerTree.Designer.cs
+++ OECP.NET/ControlStation/OECPLayerTree.Designer.cs
@@ -28,11 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OECPLayerTree));
             this.panel1 = new System.Windows.Forms.Panel();
             this.layerTree = new CCWin.SkinControl.SkinTreeView();
             this.tabControlExt1 = new HZH_Controls.Controls.TabControlExt();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.treeLayerImages = new System.Windows.Forms.ImageList(this.components);
             this.tabControlExt1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,11 +53,18 @@
             this.layerTree.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.layerTree.CheckBoxes = true;
             this.layerTree.Dock = System.Windows.Forms.DockStyle.Top;
-            this.layerTree.Indent = 20;
+            this.layerTree.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.layerTree.FullRowSelect = true;
+            this.layerTree.ImageIndex = 0;
+            this.layerTree.ImageList = this.treeLayerImages;
+            this.layerTree.Indent = 30;
             this.layerTree.ItemHeight = 25;
             this.layerTree.LineColor = System.Drawing.Color.White;
             this.layerTree.Location = new System.Drawing.Point(0, 20);
             this.layerTree.Name = "layerTree";
+            this.layerTree.SelectedImageIndex = 0;
+            this.layerTree.ShowPlusMinus = false;
+            this.layerTree.ShowRootLines = false;
             this.layerTree.Size = new System.Drawing.Size(283, 184);
             this.layerTree.TabIndex = 1;
             // 
@@ -77,23 +87,34 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.tabPage1.Location = new System.Drawing.Point(4, 34);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(275, 208);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "图层操作";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 34);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(275, 125);
+            this.tabPage2.Size = new System.Drawing.Size(275, 208);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "画布检查";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // treeLayerImages
+            // 
+            this.treeLayerImages.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("treeLayerImages.ImageStream")));
+            this.treeLayerImages.TransparentColor = System.Drawing.Color.Transparent;
+            this.treeLayerImages.Images.SetKeyName(0, "dot  black.png");
+            this.treeLayerImages.Images.SetKeyName(1, "grey line.png");
+            this.treeLayerImages.Images.SetKeyName(2, "grid.png");
+            this.treeLayerImages.Images.SetKeyName(3, "simple blue line.png");
+            this.treeLayerImages.Images.SetKeyName(4, "simple red line.png");
+            this.treeLayerImages.Images.SetKeyName(5, "layer.png");
             // 
             // OECPLayerTree
             // 
@@ -118,5 +139,6 @@
         private HZH_Controls.Controls.TabControlExt tabControlExt1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ImageList treeLayerImages;
     }
 }
--- OECP.NET/ControlStation/OECPLayerTree.cs
+++ OECP.NET/ControlStation/OECPLayerTree.cs
@@ -17,11 +17,46 @@ namespace OECP.NET.ControlStation
         public OECPLayerTree()
         {
             InitializeComponent();
+            resizeImageList();
             FormBorderStyle = FormBorderStyle.None;
+            layerTree.FullRowSelect = true;
 
-            var root = new  TreeNode("画布图层");
-            root.Nodes.Add(new TreeNode("test"));
+            var root = new  TreeNode("画布图层",5,5);
+
+            var nodeGrid = new TreeNode("网格",2,2);
+            var nodeMountainLine = new TreeNode("山线",4,4);
+            var nodeValleyLine = new TreeNode("谷线",3,3);
+            var nodeAuxLine = new TreeNode("辅助线",1,1);
+            var nodeVertex = new TreeNode("节点",0,0);
+
+            root.Nodes.AddRange(new[]
+            {
+                nodeGrid, nodeMountainLine, nodeValleyLine, nodeAuxLine, nodeVertex
+            });
+
+
             layerTree.Nodes.Add(root);
+
+            layerTree.ExpandAll();
         }
+
+        private void resizeImageList()
+        {
+            Point destPt = new Point(6, 0);
+            Size size = new Size(22, 16);
+            layerTree.ImageList = new ImageList();
+            layerTree.ImageList.ImageSize = size;
+            foreach (var imgKey in treeLayerImages.Images.Keys)
+            {
+                Bitmap bmp = new Bitmap(size.Width, size.Height);
+                Graphics g = Graphics.FromImage(bmp);
+                var img = treeLayerImages.Images[imgKey];
+                g.DrawImage(img, destPt);
+                g.Dispose();
+                layerTree.ImageList.Images.Add(imgKey, (Image)bmp);
+            }
+        }
+
+
     }
 }
